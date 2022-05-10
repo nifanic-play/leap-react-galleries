6c8e2ef8d8c6e88d3eaccdd8e86b1a5f513ab10b
nifanic <nifanic@masterpage.net>
2022-05-09 23:25:42 -0500

    refactor: Remove unused files

--- src/actions/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./api";
-export * from "./artworks";
-export * from "./actionTypes";
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { StateProvider } from "./providers/stateProvider";
-import { GlobalReducer as globalReducer, initialState } from "./reducers";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={globalReducer}>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
